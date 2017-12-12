3ed55e90d66d3b4c1d7e45baee819c65dcd03406
Sai Kiran Burle <sai@celect.com>
2017-12-12 16:59:18 -0500

    added linear

--- simple_keras.py
+++ simple_keras.py
@@ -4,7 +4,7 @@ from keras.layers import Input, Embedding, concatenate, Dense, Flatten
 from data_generator import NUM_USERS, NUM_MOVIES, get_ratings_data, BATCH_SIZE, transform_ratings_into_classes
 
 
-def get_model():
+def get_class_model():
     user = Input(shape=(1,), dtype='int32', name='input_user')
     user_embedding = Embedding(output_dim=25, input_dim=NUM_USERS)(user)
 
@@ -25,10 +25,33 @@ def get_model():
 
     return model
 
+
+def get_regression_model():
+    user = Input(shape=(1,), dtype='int32', name='input_user')
+    user_embedding = Embedding(output_dim=25, input_dim=NUM_USERS)(user)
+
+    movie = Input(shape=(1,), dtype='int32', name='input_movie')
+    movie_embedding = Embedding(output_dim=25, input_dim=NUM_MOVIES)(movie)
+
+    merged = concatenate([user_embedding, movie_embedding])
+
+    x = Flatten()(merged)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(64, activation='relu')(x)
+
+    ratings = Dense(1, name='ratings')(x)
+
+    model = Model(inputs=[user, movie], outputs=[ratings])
+    # TODO: Maybe some other metrics?
+    model.compile(optimizer='adam', loss='mse', metrics=['mse', 'mae'])
+
+    return model
+
 if __name__ == "__main__":
-    model = get_model()
+    # model = get_class_model()
     train_users, train_movies, train_ratings = get_ratings_data()
-    train_ratings = transform_ratings_into_classes(train_ratings)
+    # train_ratings = transform_ratings_into_classes(train_ratings)
+    model = get_regression_model()
     model.fit({'input_user': train_users, 'input_movie': train_movies},
               {'ratings': train_ratings},
               epochs=5, batch_size=BATCH_SIZE,
